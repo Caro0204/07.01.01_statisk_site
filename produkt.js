5dee45a345dca05f3ac977677e47d4a62dcb41a1
Caroline <cave0001@stud.ked.dk>
2023-08-31 16:59:01 +0200

    pris ændring

--- produkt.js
+++ produkt.js
@@ -14,4 +14,16 @@ function showProduct(product) {
   document.querySelector("dd.subcategory").textContent = product.subcategory;
   document.querySelector("dd.relid").textContent = product.relid;
   document.querySelector("h2.productdisplayname").textContent = product.productdisplayname;
+
+  // discount
+  if (product.discount) {
+    const discountedPrice = product.price - (product.price / 100) * product.discount;
+    document.querySelector("p.rpris").textContent = "DKK " + discountedPrice.toFixed(0); // for den til at vise uden decimaler
+    document.querySelector("p.rrabatryk").textContent = product.discount + "%";
+  } else {
+    const discountElement = copy.querySelector(".discount");
+    if (discountElement) {
+      discountElement.remove();
+    }
+  }
 }
